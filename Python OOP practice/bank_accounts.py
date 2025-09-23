b6d61ef54406df77d3cbbc82589cefbcd7632e3d
Alex Phan <105334704@student.swin.edu.au>
2025-09-23 18:56:18 +1000

    Withdraw

--- Python OOP practice/bank_accounts.py
+++ Python OOP practice/bank_accounts.py
@@ -1,3 +1,6 @@
+class BalanceException(Exception):
+    pass
+
 class BankAccount:
     def __init__(self, initialAmount, acctName):
         self.balance = initialAmount
@@ -12,3 +15,18 @@ class BankAccount:
         self.balance += amount
         print("Deposit accepted")
         self.getBalance()
+
+    def viableTransaction(self, amount):
+        if self.balance >= amount:
+            return 
+        else:
+            raise BalanceException(f"\n Sorry, {self.name}, you have insufficient funds for this transaction. Your current balance is ${self.balance:.2f}")
+        
+    def withdraw(self, amount):
+        try:
+            self.viableTransaction(amount)
+            self.balance -= amount
+            print("Withdrawal accepted")
+            self.getBalance()
+        except BalanceException as error:
+            print(f"\n withdraw interrupted: {error}")
