08176655aeac57fd100aeba8ebb28dffafbcbf99
Alex Phan <105334704@student.swin.edu.au>
2025-09-23 18:27:25 +1000

    getbalance

--- Python OOP practice/bank_accounts.py
+++ Python OOP practice/bank_accounts.py
@@ -3,3 +3,8 @@ class BankAccount:
         self.balance = initialAmount
         self.name = acctName
         print(f"\nAccount for {self.name} created with initial balance of ${self.balance:.2f}")
+
+
+    def getBalance(self):
+        print(f"\nAccount balance for {self.name} is ${self.balance:.2f}")
+        
