18aafe2f86eac08858001e8d5cc5208e3a5d147b
Alex Phan <105334704@student.swin.edu.au>
2025-09-23 18:19:16 +1000

    OOP practice

--- JavaCourse/src/Main.java
+++ JavaCourse/src/Main.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Main {
+import java.security.KeyStore;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("enter age: ");
+        int age = scanner.nextInt();
+
+        System.out.println("hello " + name);
+        System.out.println("you are " + age);
+
+        scanner.close();
+    }
 }
