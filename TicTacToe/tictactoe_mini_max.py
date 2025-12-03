1fe4c0d2b12b6324a981989b16fa16b0fe3f8290
Alex <alexphan0038@gmail.com>
2025-12-03 18:30:20 +1100

    Tictactoe game class

--- TicTacToe/tictactoe_mini_max.py
+++ TicTacToe/tictactoe_mini_max.py
@@ -14,7 +14,7 @@ class Random(Player):
     
 class Random_efficient(Player):
     def get_move(self, game):
-        available_moves = [i for i, spot in enumerate(game.cells) if cell not in ['X', 'O']]
+        available_moves = [i for i, spot in enumerate(game.cells) if cells not in ['X', 'O']]
         return random.choice(available_moves)
     
 class MinimaxPlayer(Player):
@@ -62,3 +62,61 @@ class MinimaxPlayer(Player):
                     best_score = min(score, best_score)
             return best_score
         
+class TicTacToe:
+    def __init__(self):
+        self.cells = [str(i) for i in range(1, 10)]
+        self.winner = None
+
+    def render(self):
+        print("\n-------------")
+        for i in range(0, 9, 3):
+            print(f"| {self.cells[i]} | {self.cells[i+1]} | {self.cells[i+2]} |")
+            print("-------------")
+        
+    def game_start(self, player):
+        valid = False
+        while not valid:
+            move = player.get_move(self)
+            if self.cells[move] not in ['X', 'O']:
+                self.cells[move] = player.marker
+                valid = True
+            else:
+                print("Invalid move. Please try again.")
+
+    def check_winner(self):
+        winning_combinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],  # rows
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],  # columns
+            [0, 4, 8], [2, 4, 6]              # diagonals
+        ]
+
+        for a, b , c in winning_combinations:
+            if self.cells[a] == self.cells[b] == self.cells[c]:
+                return self.cells[a]
+        
+        if all(cell in ['X', 'O'] for cell in self.cells):
+            return "tie"
+        
+        return None
+    
+
+if __name__ == "__main__":
+    game = TicTacToe()
+    player1 = MinimaxPlayer('X')
+    player2 = Random_efficient('O')
+
+    current_player = player1
+
+    while game.winner is None:
+        game.render()
+        game.game_start(current_player)
+        game.winner = game.check_winner()
+        if game.winner:
+            break
+        current_player = player1 if current_player == player2 else player2
+
+    game.render()
+    if game.winner == "tie":
+        print("The game is a tie!")
+    else:
+        print(f"Player {game.winner} wins!")
